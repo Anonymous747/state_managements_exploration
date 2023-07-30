ff038f8489c303cfb364c21c77424cd6ccc6a769
Mike <kobrinets2015@mail.ru>
2023-07-30 18:38:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# state_managements_exploration
+ A quick and elegant demonstration of the most popular state management concepts
---
+++ android/app/src/main/kotlin/com/example/state_managements_exploration/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.state_managements_exploration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
